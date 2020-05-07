15c9e154b05c6e7f957a35e7734ca1118578e099
Michael <m.molin.1987@gmail.com>
2020-05-07 09:20:58 +0200

    Pulizia codice

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,9 @@
 <header>
     <img id="logo" src="{{asset('img/marchio-sito-test.png')}}" alt="Logo Pasta Molisana">
     <ul class="nav-menu">
-        <li><a href="{{route('home')}}">{!! '<h3>Home</h3>' !!}</a></li>
-        <li><a href="{{route('prodotti')}}">{!! '<h3>Prodotti</h3>' !!}</a></li>
-        <li><a href="{{route('news')}}">{!! '<h3>News</h3>' !!}</a></li>
+        <li class="{{(url() -> current() == route('home')) ? 'active' : ''}}"><a href="{{route('home')}}">{!! '<h3>Home</h3>' !!}</a></li>
+        <li class="{{(url() -> current() == route('prodotti')) ? 'active' : ''}}"><a href="{{route('prodotti')}}">{!! '<h3>Prodotti</h3>' !!}</a></li>
+        <li class="{{(url() -> current() == route('news')) ? 'active' : ''}}"><a href="{{route('news')}}">{!! '<h3>News</h3>' !!}</a></li>
     </ul>
     <br>
     <img id="hamburger" src="{{asset('img/hambuger.svg')}}" alt="">
